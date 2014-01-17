dd805d85fec2d4051cb92dc23eb5a269dd8cad79
Aaron Lidman <aaronlidman@gmail.com>
2014-01-17 17:32:15 -0500

    Add more plugins

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,21 @@ s3cmd put --acl-public --mime-type "image/png" plugins/leaflet-draw/v0.2.2/image
 s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster.js s3://mapbox-js/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster.js
 s3cmd put --acl-public --mime-type "text/css" plugins/leaflet-markercluster/v0.4.0/MarkerCluster.Default.css s3://mapbox-js/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.Default.css
 s3cmd put --acl-public --mime-type "text/css" plugins/leaflet-markercluster/v0.4.0/MarkerCluster.css s3://mapbox-js/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.css
+s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-label/v0.2.1/leaflet.label.js s3://mapbox-js/mapbox.js/plugins/leaflet-label/v0.2.1/leaflet.label.js
+s3cmd put --acl-public --mime-type "text/css" plugins/leaflet-label/v0.2.1/leaflet.label.css s3://mapbox-js/mapbox.js/plugins/leaflet-label/v0.2.1/leaflet.label.css
+s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-image/v0.0.3/leaflet-image.js s3://mapbox-js/mapbox.js/plugins/leaflet-image/v0.0.3/leaflet-image.js
+s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-locatecontrol/v0.21.0/L.Control.Locate.js s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/L.Control.Locate.js
+s3cmd put --acl-public --mime-type "text/css" plugins/leaflet-locatecontrol/v0.21.0/L.Control.Locate.ie.css s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/L.Control.Locate.ie.css
+s3cmd put --acl-public --mime-type "text/css" plugins/leaflet-locatecontrol/v0.21.0/L.Control.Locate.css s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/L.Control.Locate.css
+s3cmd put --acl-public --mime-type "text/css" plugins/leaflet-locatecontrol/v0.21.0/L.Control.Locate.0.5.css s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/L.Control.Locate.0.5.css
+s3cmd put --acl-public --mime-type "image/png" plugins/leaflet-locatecontrol/v0.21.0/images/locate_alt.png s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/images/locate_alt.png
+s3cmd put --acl-public --mime-type "image/svg+xml" plugins/leaflet-locatecontrol/v0.21.0/images/locate_alt.svg s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/images/locate_alt.svg
+s3cmd put --acl-public --mime-type "image/png" plugins/leaflet-locatecontrol/v0.21.0/images/locate_touch_alt.png s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/images/locate_touch_alt.png
+s3cmd put --acl-public --mime-type "image/png" plugins/leaflet-locatecontrol/v0.21.0/images/locate_touch.png s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/images/locate_touch.png
+s3cmd put --acl-public --mime-type "image/png" plugins/leaflet-locatecontrol/v0.21.0/images/locate.png s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/images/locate.png
+s3cmd put --acl-public --mime-type "image/svg+xml" plugins/leaflet-locatecontrol/v0.21.0/images/locate.svg s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/images/locate.svg
+s3cmd put --acl-public --mime-type "image/gif" plugins/leaflet-locatecontrol/v0.21.0/images/spinner.gif s3://mapbox-js/mapbox.js/plugins/leaflet-locatecontrol/v0.21.0/images/spinner.gif
+s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-osm/v0.1.0/leaflet-osm.js s3://mapbox-js/mapbox.js/plugins/leaflet-osm/v0.1.0/leaflet-osm.js
 echo ""
 echo ""
 echo "--- DEPLOYED mapbox.js plugins ----"
