6f92d8a6d7ecf308bd910a64e71f0878f67e4ae1
Tom MacWright <tom@macwright.org>
2014-05-08 12:34:28 -0400

    Add omnivore bump

--- deploy.sh
+++ deploy.sh
@@ -67,6 +67,8 @@ s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-heat
 s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-omnivore/v0.0.0/leaflet-omnivore.min.js s3://mapbox-js/mapbox.js/plugins/leaflet-omnivore/v0.0.0/leaflet-omnivore.min.js
 s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-omnivore/v0.0.1/leaflet-omnivore.min.js s3://mapbox-js/mapbox.js/plugins/leaflet-omnivore/v0.0.1/leaflet-omnivore.min.js
 s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-omnivore/v0.1.0/leaflet-omnivore.min.js s3://mapbox-js/mapbox.js/plugins/leaflet-omnivore/v0.1.0/leaflet-omnivore.min.js
+s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-omnivore/v0.1.1/leaflet-omnivore.min.js s3://mapbox-js/mapbox.js/plugins/leaflet-omnivore/v0.1.1/leaflet-omnivore.min.js
+s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-omnivore/v0.1.1/leaflet-omnivore.js s3://mapbox-js/mapbox.js/plugins/leaflet-omnivore/v0.1.1/leaflet-omnivore.js
 s3cmd put --acl-public --mime-type "application/javascript" plugins/arc.js/v0.1.0/arc.js s3://mapbox-js/mapbox.js/plugins/arc.js/v0.1.0/arc.js
 s3cmd put --acl-public --mime-type "text/css" plugins/leaflet-minimap/v1.0.0/Control.MiniMap.css s3://mapbox-js/mapbox.js/plugins/leaflet-minimap/v1.0.0/Control.MiniMap.css
 s3cmd put --acl-public --mime-type "application/javascript" plugins/leaflet-minimap/v1.0.0/Control.MiniMap.js s3://mapbox-js/mapbox.js/plugins/leaflet-minimap/v1.0.0/Control.MiniMap.js
